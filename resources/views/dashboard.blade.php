8464d450501211b6c5d69c5b1559ba279c28e097
margleb <margleb.dm@yandex.ru>
2022-12-12 16:18:45 +0300

    Добавлены карточки мероприятий

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,216 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+    <div class="container my-12 mx-auto px-4 md:px-12">
+        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+
+            <!-- колонка -->
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                <article class="overflow-hidden rounded-lg shadow-lg">
+
+                    <a href="#">
+                        <img alt="Placeholder" class="block h-auto w-full" src="images/vdnh.jpg">
+                    </a>
+
+                    <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+                        <h1 class="text-lg">
+                            <a class="no-underline hover:underline text-black" href="#">
+                                Встреча на ВДНХ
+                            </a>
+                        </h1>
+                        <p class="text-grey-darker text-sm">
+                            12.12.2022 14:00
+                        </p>
+                    </header>
+
+                    <div class="p-2 md:p-4">
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                            Встечаемся на ВДНХ у памятника "Рабочей и Колхознице" и знакомимся...
+                        </p>
+                        <p class="text-grey-darker text-sm">
+                            Кол-во участников: 50 <br> Девушек - 20 <br> Парней - 30
+                        </p>
+                    </div>
+
+                    <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Я приду
+                        </a>
+                    </footer>
+                </article>
             </div>
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                <article class="overflow-hidden rounded-lg shadow-lg">
+
+                    <a href="#">
+                        <img alt="Placeholder" class="block h-auto w-full" src="images/vdnh.jpg">
+                    </a>
+
+                    <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+                        <h1 class="text-lg">
+                            <a class="no-underline hover:underline text-black" href="#">
+                                Встреча на ВДНХ
+                            </a>
+                        </h1>
+                        <p class="text-grey-darker text-sm">
+                            12.12.2022 14:00
+                        </p>
+                    </header>
+
+                    <div class="p-2 md:p-4">
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                            Встечаемся на ВДНХ у памятника "Рабочей и Колхознице" и знакомимся...
+                        </p>
+                        <p class="text-grey-darker text-sm">
+                            Кол-во участников: 50 <br> Девушек - 20 <br> Парней - 30
+                        </p>
+                    </div>
+
+                    <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Я приду
+                        </a>
+                    </footer>
+                </article>
+            </div>
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                <article class="overflow-hidden rounded-lg shadow-lg">
+
+                    <a href="#">
+                        <img alt="Placeholder" class="block h-auto w-full" src="images/vdnh.jpg">
+                    </a>
+
+                    <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+                        <h1 class="text-lg">
+                            <a class="no-underline hover:underline text-black" href="#">
+                                Встреча на ВДНХ
+                            </a>
+                        </h1>
+                        <p class="text-grey-darker text-sm">
+                            12.12.2022 14:00
+                        </p>
+                    </header>
+
+                    <div class="p-2 md:p-4">
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                            Встечаемся на ВДНХ у памятника "Рабочей и Колхознице" и знакомимся...
+                        </p>
+                        <p class="text-grey-darker text-sm">
+                            Кол-во участников: 50 <br> Девушек - 20 <br> Парней - 30
+                        </p>
+                    </div>
+
+                    <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Я приду
+                        </a>
+                    </footer>
+                </article>
+            </div>
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                <article class="overflow-hidden rounded-lg shadow-lg">
+
+                    <a href="#">
+                        <img alt="Placeholder" class="block h-auto w-full" src="images/vdnh.jpg">
+                    </a>
+
+                    <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+                        <h1 class="text-lg">
+                            <a class="no-underline hover:underline text-black" href="#">
+                                Встреча на ВДНХ
+                            </a>
+                        </h1>
+                        <p class="text-grey-darker text-sm">
+                            12.12.2022 14:00
+                        </p>
+                    </header>
+
+                    <div class="p-2 md:p-4">
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                            Встечаемся на ВДНХ у памятника "Рабочей и Колхознице" и знакомимся...
+                        </p>
+                        <p class="text-grey-darker text-sm">
+                            Кол-во участников: 50 <br> Девушек - 20 <br> Парней - 30
+                        </p>
+                    </div>
+
+                    <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Я приду
+                        </a>
+                    </footer>
+                </article>
+            </div>
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                <article class="overflow-hidden rounded-lg shadow-lg">
+
+                    <a href="#">
+                        <img alt="Placeholder" class="block h-auto w-full" src="images/vdnh.jpg">
+                    </a>
+
+                    <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+                        <h1 class="text-lg">
+                            <a class="no-underline hover:underline text-black" href="#">
+                                Встреча на ВДНХ
+                            </a>
+                        </h1>
+                        <p class="text-grey-darker text-sm">
+                            12.12.2022 14:00
+                        </p>
+                    </header>
+
+                    <div class="p-2 md:p-4">
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                            Встечаемся на ВДНХ у памятника "Рабочей и Колхознице" и знакомимся...
+                        </p>
+                        <p class="text-grey-darker text-sm">
+                            Кол-во участников: 50 <br> Девушек - 20 <br> Парней - 30
+                        </p>
+                    </div>
+
+                    <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Я приду
+                        </a>
+                    </footer>
+                </article>
+            </div>
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                <article class="overflow-hidden rounded-lg shadow-lg">
+
+                    <a href="#">
+                        <img alt="Placeholder" class="block h-auto w-full" src="images/vdnh.jpg">
+                    </a>
+
+                    <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+                        <h1 class="text-lg">
+                            <a class="no-underline hover:underline text-black" href="#">
+                                Встреча на ВДНХ
+                            </a>
+                        </h1>
+                        <p class="text-grey-darker text-sm">
+                            12.12.2022 14:00
+                        </p>
+                    </header>
+
+                    <div class="p-2 md:p-4">
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                            Встечаемся на ВДНХ у памятника "Рабочей и Колхознице" и знакомимся...
+                        </p>
+                        <p class="text-grey-darker text-sm">
+                            Кол-во участников: 50 <br> Девушек - 20 <br> Парней - 30
+                        </p>
+                    </div>
+
+                    <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Я приду
+                        </a>
+                    </footer>
+                </article>
+            </div>
+
         </div>
     </div>
+
 </x-app-layout>
