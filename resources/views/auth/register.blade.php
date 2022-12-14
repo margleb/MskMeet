134f33de23b24162e0664d133bde2ef0663668b7
vagrant <vagrant@homestead>
2022-12-14 20:08:45 +0000

    Добавлены поля для регистриции. Добавлена библиотека Flowbite

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,9 @@
+
+@push('scripts')
+    <script src="https://unpkg.com/flowbite@1.5.1/dist/flowbite.js"></script>
+    <script src="https://unpkg.com/flowbite@1.5.5/dist/datepicker.js"></script>
+@endpush
+
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
@@ -46,14 +52,36 @@
                 <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
             </div>
 
-                <!-- Avatar -->
+            <!-- Secret Key -->
+            <div class="mt-4">
+                <x-input-label for="password_confirmation" :value="__('Secret Key')" />
+
+                <x-text-input id="secretKey" class="block mt-1 w-full" type="text" name="secretKey" required autofocus />
+            </div>
+
+
+            <!-- Date of birth -->
+            <div class="mt-4">
+                <x-input-label for="password_confirmation" :value="__('BirthDate')" />
+
+                <x-datepicker />
+            </div>
+
+            <!-- Sex -->
+            <div class="mt-4">
+                <x-input-label for="sex" :value="__('Sex')" />
+
+                <x-radio type="radio" id="male" name="sex" label="Мужской"  mb="mb-4"/>
+                <x-radio type="radio" id="female" name="sex" label="Женский" />
+            </div>
+
+            <!-- Avatar -->
             <div class="mt-4">
                 <x-input-label for="avatar" :value="__('Avatar (optional)')" />
 
                 <x-text-input id="avatar" class="block mt-1 w-full"
                               type="file"
                               name="avatar" required />
-
             </div>
 
             <div class="flex items-center justify-end mt-4">
